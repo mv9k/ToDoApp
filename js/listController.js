c510ce2d1d3b4c61f0b846a921312e0967f321d6
Kris <mv9kjw@gmail.com>
2015-12-15 20:57:54 -0700

    added finishTask and delTask, and changed styling.

--- js/listController.js
+++ js/listController.js
@@ -10,30 +10,40 @@
 
         // list everything
         var lc = this;
-        lc.task = '';
         lc.listname = 'default value';
         //lc.sortTasks = sortTasks;
         lc.tasks = [
             {
                 name: 'first',
-                completed: false
+                finished: false
             },
             {
                 name: 'second',
-                completed: false
+                finished: false
             }];
 
         lc.addTask = addTask;
+        lc.delTask = delTask;
+        lc.finishTask = finishTask;
 
 
         //// define functions
         //function sortTasks() {
         //    lc.tasks = $filter('orderBy')(lc.tasks, 'priority');
         //}
+        function finishTask(task) {
+            task.finished = true;
+        }
+
         function addTask(task) {
             lc.tasks.push(task);
 
         }
+
+        function delTask(index) {
+            lc.tasks.splice(index, 1);
+        }
+
     }
 
 }());
