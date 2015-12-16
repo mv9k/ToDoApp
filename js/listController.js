68e2ccbcc5768514229f8051944c72456cd9f9b4
Kris <mv9kjw@gmail.com>
2015-12-16 10:58:44 -0700

    Added multiple lists functionality.

--- js/listController.js
+++ js/listController.js
@@ -10,17 +10,10 @@
 
         // list everything
         var lc = this;
-        lc.listname = 'default value';
+        //lc.listname = 'default value';
         //lc.sortTasks = sortTasks;
-        lc.tasks = [
-            {
-                name: 'first',
-                finished: false
-            },
-            {
-                name: 'second',
-                finished: false
-            }];
+        lc.tasks = [];
+        lc.lists = [];
 
         lc.addTask = addTask;
         lc.delTask = delTask;
@@ -37,8 +30,13 @@
         }
 
         function addTask(task) {
-            lc.tasks.push({'name': task.name, 'finished': false});
 
+            lc.tasks.push({'name': task.name, 'listname': task.listname, 'finished': false});
+
+            // checks for duplicate list name in lists array before adding to it
+            if(lc.lists.indexOf(task.listname) === -1) {
+                lc.lists.push(task.listname);
+            }
         }
 
         function delTask(index) {
