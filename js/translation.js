3a41b842f1524ed1337eef63eae0582a4976bfc0
Kris <mv9kjw@gmail.com>
2016-01-17 22:18:33 -0700

    still working on languages

--- js/translation.js
+++ js/translation.js
@@ -3,12 +3,16 @@
 
     angular.module('translationfile', ['pascalprecht.translate'])
         .config(function($translateProvider) {
-            $translateProvider.translations('en', {
-                AppTitle: 'toDo App',
-                Home: 'Home',
-                About: 'About'
+            $translateProvider
+
+                .translations('en', {
+                    AppTitle: 'toDo App',
+                    Home: 'Lists',
+                    About: 'About',
+                    ShowAllLists: 'Show All Lists',
+                    ClearFinishedTasks: 'Clear Finished Tasks'
             })
-                .translations('de', {
+                .translations('jp', {
                     AppTitle: 'De toDo App',
                     Home: 'De Home',
                     About: 'De About'
